83c041246fb7b21a8abf8af52f34a9b94cb035fd
Marcel Reiter <marcel.reiter02@gmai.com>
2024-11-06 14:59:32 +0100

    Revert "Added some more cool Keybinds"
    This reverts commit c99308b315d903fb127e7376249231153e65e7c2.

--- init.lua
+++ init.lua
@@ -389,8 +389,7 @@ require('lazy').setup({
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>ga'] = { name = '[G]it [A]dd', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it Hunk', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
         ['<leader>o'] = { name = '[O]pen Nvim plugins', _ = 'which_key_ignore' },
       }
--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -5,7 +5,7 @@ return {
   vim.keymap.set('n', '<leader>ga.', '<cmd>!git add . <CR>', { desc = '[G]it [A]dd' }),
   vim.keymap.set('n', '<leader>gc', '<cmd>!git commit <CR>', { desc = '[G]it [C]ommit' }),
   vim.keymap.set('n', '<leader>gac', '<cmd>!git add . && git commit <CR>', { desc = '[G]it [A]dd and [C]ommit' }),
-  vim.keymap.set('n', '<leader>gp', '<cmd>!git push origin master <CR>', { desc = '[G]it [P}ush' }),
-  vim.keymap.set('n', '<leader>om', '<cmd>Mason <CR>', { desc = '[O]pen [M]ason', silent = false }),
-  vim.keymap.set('n', '<leader>ol', '<cmd>Lazy <CR>', { desc = '[O]pen [L]azy', silent = false }),
+<<<<<<< HEAD
+=======
+>>>>>>> parent of c99308b (Added some more cool Keybinds)
 }
